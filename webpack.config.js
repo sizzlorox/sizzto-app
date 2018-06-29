1e19d220c00569cc4224cfb3e4b584b66ee4baac
Sunny Huang <sizzlorox@gmail.com>
2018-06-29 09:49:08 -0300

    chore(webpack): removed hot module plugin from prod build

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,6 @@ const options = {
       filename: 'index.html'
     }),
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
