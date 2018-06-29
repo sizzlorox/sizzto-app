db716661bceb99bc18452ddd3bc6bb56f41836e2
Sunny Huang <sizzlorox@gmail.com>
2018-06-29 15:59:40 -0300

    feat(App): added infinitescroll

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 // Components
 const Navbar = require('./components/nav-bar/Navbar');
 const Header = require('./components/header/Header');
-const Card = require('./components/card/Card');
+const InfiniteScroll = require('./components/infinite-scroll/InfiniteScroll');
 
 class App extends React.Component {
   // Should initialize state in constructor instead of getInitialState when using ES6 Classes
@@ -33,44 +33,6 @@ class App extends React.Component {
 
   componentDidMount() {
     this.setState({ isLoading: false });
-    fetch('https://api.coinmarketcap.com/v1/ticker/?limit=15')
-      .then(results => results.json())
-      .then(data => {
-        console.log(data);
-        const cryptoData = data.map((crypto, index) => {
-          return (
-            <Card key={index} title={`${crypto.name} (${crypto.symbol})`}>
-              <section>
-                {`Rank: ${crypto.rank}`}
-                <br />
-                {`Price: $${crypto.price_usd}`}
-                <br />
-                {`Price to BTC: ${crypto.price_btc}`}
-                <br />
-                {`Change 1h: ${crypto.percent_change_1h > 0 ? '\u2191' : '\u2193'} ${crypto.percent_change_1h}%`}
-                <br />
-                {`Change 24h: ${crypto.percent_change_24h > 0 ? '\u2191' : '\u2193'} ${crypto.percent_change_24h}%`}
-                <br />
-                {`Change 7d: ${crypto.percent_change_7d > 0 ? '\u2191' : '\u2193'} ${crypto.percent_change_7d}%`}
-                <hr />
-                {`Last Updated: ${this.formatTime(crypto.last_updated)}`}
-              </section>
-            </Card>
-          );
-        });
-
-        this.setState({
-          cryptoData
-        });
-      });
-  }
-
-  formatTime(unix_timestamp) {
-    const date = new Date(unix_timestamp * 1000);
-    const hours = date.getHours();
-    const minutes = "0" + date.getMinutes();
-    const seconds = "0" + date.getSeconds();
-    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
   }
 
   render() {
@@ -86,9 +48,7 @@ class App extends React.Component {
             <Navbar items={this.state.navbar} />
           </div>
           <Header text='Sizzto' subheader='Crypto-Currency Info' />
-          <div id='card-list'>
-            {this.state.cryptoData}
-          </div>
+          <InfiniteScroll />
         </div>
       )
   }
---
+++ src/app/components/infinite-scroll/InfiniteScroll.js
@@ -0,0 +1,91 @@
+// Components
+const Card = require('../card/Card');
+
+class InfiniteScroll extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      start: 0,
+      limit: 30
+    };
+
+    this.listenScrollEvent = this.listenScrollEvent.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+    window.addEventListener('scroll', this.listenScrollEvent);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.listenScrollEvent);
+  }
+
+  listenScrollEvent() {
+    if (window.pageYOffset >= (document.body.scrollHeight - document.body.clientHeight)) {
+      this.setState({
+        start: this.state.start + this.state.limit,
+
+      });
+      this.fetchData();
+    }
+  }
+
+  fetchData() {
+    this.setState({
+      isLoading: false,
+    });
+    fetch(`https://api.coinmarketcap.com/v2/ticker/?start=${this.state.start}&limit=${this.state.limit}`)
+      .then(results => results.json())
+      .then(result => {
+        const { data } = result;
+        return Object.keys(data).sort((key2, key1) => data[key2].rank - data[key1].rank).map((key) => {
+          return (
+            <Card key={key + this.state.start} title={`${data[key].name} (${data[key].symbol})`}>
+              <section>
+                {`Rank: ${data[key].rank}`}
+                <br />
+                {`Price: $${data[key].quotes['USD'].price}`}
+                <br />
+                {`Change 1h: ${data[key].quotes['USD'].percent_change_1h > 0 ? '\u2191' : '\u2193'} ${data[key].quotes['USD'].percent_change_1h}%`}
+                <br />
+                {`Change 24h: ${data[key].quotes['USD'].percent_change_24h > 0 ? '\u2191' : '\u2193'} ${data[key].quotes['USD'].percent_change_24h}%`}
+                <br />
+                {`Change 7d: ${data[key].quotes['USD'].percent_change_7d > 0 ? '\u2191' : '\u2193'} ${data[key].quotes['USD'].percent_change_7d}%`}
+                <hr />
+                {`Last Updated: ${this.formatTime(data[key].last_updated)}`}
+              </section>
+            </Card>
+          );
+        })
+      })
+      .then((cryptoData) => {
+        this.setState({
+          isLoading: false,
+          cryptoData: this.state.cryptoData ? this.state.cryptoData.concat(cryptoData) : cryptoData
+        });
+      });
+
+  }
+
+  formatTime(unix_timestamp) {
+    const date = new Date(unix_timestamp * 1000);
+    const hours = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    const seconds = "0" + date.getSeconds();
+    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  }
+
+  render() {
+    return (
+      <div id='card-list' ref='iScroll' >
+        {this.state.cryptoData}
+        {this.state.isLoading ? <p>Loading...</p> : ''}
+      </div >
+    );
+  }
+
+}
+module.exports = InfiniteScroll;
