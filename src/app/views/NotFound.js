fee993724d1b2f3ac2b7a544943c4586e3fa499e
Sunny Huang <sizzlorox@gmail.com>
2018-07-03 13:35:04 -0300

    chore(notFound): updated 404 page

--- src/app/views/NotFound.js
+++ src/app/views/NotFound.js
@@ -1,12 +1,27 @@
 const React = require('react');
+const withRouter = require('react-router-dom');
 
 class NotFound extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.props.history.push('/');
+  }
 
   render() {
     return (
       <React.Fragment>
-        <h1>Uh oh! Something has gone wrong!</h1>
-        <p>Error 404: Page not found!</p>
+        <div className={classnames('not-found')}>
+          <h1>Uh oh! Something has gone wrong!</h1>
+          <p>Error 404: Page not found!</p>
+          <button className={classnames('submit')} type='button' onClick={this.onClick}>
+            Back Home
+          </button>
+        </div>
       </React.Fragment>
     );
   }
