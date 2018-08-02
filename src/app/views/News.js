b8f6bf776ea899dca9639af2870d22ad2c65ba63
sizzlorox <sizzlorox@gmail.com>
2018-08-02 22:55:19 +0800

    fix(news): added netlify check

--- src/app/views/News.js
+++ src/app/views/News.js
@@ -6,10 +6,12 @@ class News extends React.Component {
 
   constructor(props) {
     super(props);
+    // TODO: fix rssFeed when removing out of netlify
     this.state = {
       title: 'Cyrpto-News (No styling yet)',
       rssFeed: process.env.NODE_ENV.includes('production')
-        ? '//cryptscout.com/cryptocurrency-news-rss.php'
+        ? process.env.NODE_ENV.includes('netlify') ? 'https://cors-anywhere.herokuapp.com/http://cryptscout.com/cryptocurrency-news-rss.php'
+          : '//cryptscout.com/cryptocurrency-news-rss.php'
         : 'https://cors-anywhere.herokuapp.com/http://cryptscout.com/cryptocurrency-news-rss.php'
     };
   }
